e17c3fa58dc7a3b8c17cda4080aa4ec863f6f38f
Antonio Herrera <herreraquinteroj@gmail.com>
2025-01-10 15:29:54 -0800

    instalaciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [mdx()],
+    output: "server",
+    adapter: node({
+      mode: "standalone"
+    })
+});
