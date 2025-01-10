2f37eabd7667cb5b1db1fd5299d585fa3415ea4e
Antonio Herrera <herreraquinteroj@gmail.com>
2025-01-10 15:31:30 -0800

    borre node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx()],
-    output: "server",
-    adapter: node({
-      mode: "standalone"
-    })
 });
